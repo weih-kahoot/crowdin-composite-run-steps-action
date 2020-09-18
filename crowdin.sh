dfa639fce92674565a5d9053450c38575e485491
Wei Huang <weih@kahoot.com>
2020-09-19 00:26:20 +0200

    W

--- crowdin.sh
+++ crowdin.sh
@@ -1,20 +1,29 @@
 token=$1
 project_id=$2
-file=$3
+file_id=$3
+file=$4
 
 echo "Crowdin.sh"
 
 echo "Upload file $file"
 
-curl --request POST \
+filename=$(basename $file)
+
+storage_id=$(curl --request POST \
   --url https://crowdin.com/api/v2/storages \
   --header "authorization: Bearer $token" \
   --header 'content-type: application/octet-stream' \
-  --header "crowdin-api-filename: $file" \
-  --data @$file
+  --header "crowdin-api-filename: $filename" \
+  --data @$file | jq '.data.id')
 
-# ls
+echo "Upload file as $storage_id"
 
-# date
+curl --request PUT \
+  --url "https://crowdin.com/api/v2/projects/${project_id}/files/${file_id}"
+  --header "authorization: Bearer $token" \
+  --header 'content-type: application/json' \
+  --data "{\"storageId\": $storage_id}"
+
+
 
 
